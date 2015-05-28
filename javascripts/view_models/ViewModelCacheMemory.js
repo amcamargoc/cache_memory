890c1021ba15c48a52da464a5a29a0eb27e0b263
Alberto Camargo Castro <amcamargo95@gmail.com>
2015-05-28 03:17:08 -0500

    not working miss and hit

--- javascripts/view_models/ViewModelCacheMemory.js
+++ javascripts/view_models/ViewModelCacheMemory.js
@@ -8,8 +8,10 @@ function Cache(size) {
   
   // Methods
   self.fill_blocks = function() {
-    for (i = 0; i < self.size; i++) {
-      self.blocks.push(new Block(i))
+    if (self.size <= 1024) {
+      for (i = 0; i < self.size; i++) {
+        self.blocks.push(new Block(i))
+      }
     }
   }
 }
@@ -28,8 +30,50 @@ function RamAddress(ram_address) {
 
   // Attributes
   self.ram_address = ram_address
-  state = null
-  block_tag = null  
+  self.state = null
+  self.block_tag = null
+}
+
+function CacheMemoryManagement(cache_size, ram_addresses) {
+  self = this
+
+  // Attributes
+  self.cache = new Cache(cache_size)
+  self.ram_addresses = []
+
+  // 'Constructors' function for ram_addresses attribute
+  self.cache.fill_blocks()
+  for (i in ram_addresses) { 
+    self.ram_addresses.push(new RamAddress(ram_addresses[i].trim()))
+  }
+
+  // Methods of classes
+  self.direct_mapping = function() {
+    for (i in self.ram_addresses) {
+      ram_address = self.ram_addresses[i].ram_address
+      console.log(self.ram_addresses[i])
+      var st = (is_hit(ram_address) == true) ? 'hit' : 'miss';
+      console.log(self.ram_addresses[i].state())
+      cache_address = Number(ram_address) % Number(self.cache.size)
+      
+
+      if (isNaN(cache_address)) {
+        alert('Inputs incorrectos (verficar el tamaño de la cache o las direcciones requeridas)')
+      } else {
+        console.log(cache_address)
+      }
+    }
+  }
+
+  // 'privates' Methods
+  function is_hit(ram_address) {
+    for (i in self.cache.blocks) {
+      if (self.cache.blocks[i].ram_address == ram_address) {
+        return true
+      }
+    }
+    return false
+  }
 }
 
 
@@ -39,19 +83,17 @@ function CacheMemoryVM() {
   var self = this
 
   // Attributes
-  self.ram = []
-  self.size = ko.observable()
-  self.ram_address = ko.observable()
+  self.cache_size = ko.observable(0)
+  self.ram_addresses = ko.observable('')
   
-  self.cache = ko.computed(function() {
-    object = new Cache(self.size())
-    object.fill_blocks()
-    return object
+  self.addresses_array = ko.computed(function() {
+    return self.ram_addresses().trim().split('\n')
   })
 
-  self.ram_address = ko.computed(function() {
-    
-  })
+  self.cache_management = function() {
+    cache_management = new CacheMemoryManagement(self.cache_size(), self.addresses_array())
+    cache_management.direct_mapping()
+  }
 }
 
 control = new CacheMemoryVM()
