40ee9c6e6c656906caecbc46ab90b08c8ea9e377
Alberto Camargo Castro <amcamargo95@gmail.com>
2015-06-02 14:38:29 -0500

    all access methods working

--- javascripts/view_models/ViewModelCacheMemory.js
+++ javascripts/view_models/ViewModelCacheMemory.js
@@ -34,7 +34,7 @@ function RamAddress(ramAddress) {
   self.blockTag = null
 }
 
-function CacheMemoryManagement(cache_size, ramAddresses) {
+function CacheMemoryManagement(cache_size, ramAddresses, sets) {
   self = this
 
   // Attributes
@@ -44,8 +44,39 @@ function CacheMemoryManagement(cache_size, ramAddresses) {
 
   // 'Constructors' function for ramAddresses attribute
   self.cache.fill_blocks()
-  for (i in ramAddresses) { 
+  for (var i in ramAddresses) { 
     self.ramAddresses.push(new RamAddress(ramAddresses[i].trim()))
+  }
+
+  self.saveInSets = function() {
+    groupInSets = []
+    var start = 0, countSet = 0, index = 0
+    for (var i = 0; i < parseInt(cache_size / sets); i++, index += sets) {
+      blocksSet = []
+      for (var element = index; element < index +sets; element++) {
+        blocksSet.push(self.cache.blocks[element])
+      }
+      groupInSets[countSet] = blocksSet
+      countSet += 1
+      start += 2
+    }
+    return groupInSets
+  }
+
+  self.emptySet = function(requestAddress, set, time) {
+    var sets = self.saveInSets()
+    for (var i in sets[set]) {
+      if (sets[set][i].ramAddress === null) {
+        sets[set][i].uses = time
+        return parseInt(i)
+      } else {
+        if (sets[set][i].ramAddress === requestAddress) {
+          sets[set][i].uses = time
+          return parseInt(i)
+        } 
+      }
+    }
+    return sets[set]
   }
 
   // Methods of classes
@@ -64,6 +95,43 @@ function CacheMemoryManagement(cache_size, ramAddresses) {
         self.iterations.push(self.ramAddresses[i])
       }
     }
+  }
+
+  self.associativeCache = function(isFully) {
+    self.iterations = []
+    for (var i in self.ramAddresses) {
+      ramAddress = self.ramAddresses[i].ramAddress
+      self.ramAddresses[i].state = is_hit(ramAddress) ? 'hit' : 'miss'
+      var blocksSet = isFully? 0 : ramAddress % sets
+      blockEmpty = self.emptySet(ramAddress, blocksSet, parseInt(i))
+      if(self.ramAddresses[i].state == 'miss') {
+        if ($.isNumeric(blockEmpty)) {
+          self.ramAddresses[i].blockTag = blocksSet + blockEmpty
+          self.cache.blocks[blocksSet + blockEmpty].ramAddress = ramAddress
+          //self.ramAddresses[blocksSet][blockEmpty].blockTag = cacheAddress
+          //self.ramAddresses[i].blockTag = cacheAddress
+        } else {
+          var lru = []
+          for(var j in blockEmpty) {
+            lru.push(blockEmpty[j].uses)
+          }
+          lru.sort(function(a, b){ return a-b })
+          for(var j = 0; j < blockEmpty.length; j++) {
+            if (blockEmpty[j].uses === lru[0]) {
+              self.cache.blocks[blocksSet + j].ramAddress = ramAddress
+              self.cache.blocks[blocksSet + j].uses = i
+              self.ramAddresses[i].blockTag = blocksSet + j
+              break;
+            }
+          }
+        }
+      } else {
+        self.ramAddresses[i].blockTag = blocksSet + blockEmpty
+        self.cache.blocks[blocksSet + blockEmpty].ramAddress = ramAddress
+      }
+      self.iterations.push(self.ramAddresses[i])
+    }
+    console.log(self.iterations)
   }
 
   // 'privates' Methods
@@ -90,11 +158,12 @@ function CacheMemoryVM() {
   self.change = true
   self.cacheSize = ko.observable(4)
   self.sets = ko.observable(2)
-  self.ramAddresses = ko.observable("0\n0\n0")
+  self.ramAddresses = ko.observable("0\n8\n0\n6\n8")
   self.cacheMemory = ko.observableArray()
   self.contentCacheTable = ko.observableArray()
   self.iterateRam = ko.observableArray()
-      
+  self.gameType = ko.observable('')
+
   self.addressesArray = ko.computed(function() {
     return self.ramAddresses().trim().split('\n')
   })
@@ -111,10 +180,8 @@ function CacheMemoryVM() {
   self.updateRam = ko.computed(function() {
     self.iterateRam([])
     console.log(self.iterateRam())
-    for (var i in self.addressesArray()) {
-      console.log(i)
-      self.iterateRam.push({data: self.addressesArray()[i], active: (self.currentAddress == i) ? true : false })
-      console.log(self.iterateRam())
+    for(var j = 0; j < self.addressesArray().length; j++) {
+      self.iterateRam.push({data: self.addressesArray()[j], active: (self.currentAddress === j) ? true : false })
     }
   })
 
@@ -123,7 +190,9 @@ function CacheMemoryVM() {
   }
 
   self.cacheDirect = function() {
-    cacheManagement = new CacheMemoryManagement(self.cacheSize(), self.addressesArray())
+    self.gameType('Direct')
+    self.contentCacheTable([])
+    cacheManagement = new CacheMemoryManagement(parseInt(self.cacheSize()), self.addressesArray(), self.sets())
     cacheManagement.directMapping()
     self.iterateOnCache()
     document.getElementById('menu').className = 'hide'
@@ -131,7 +200,25 @@ function CacheMemoryVM() {
     document.getElementById('cacheGame').className = 'show'
   }
 
+  self.cacheAssociative = function() {
+    self.contentCacheTable([])
+    cacheManagement = new CacheMemoryManagement(parseInt(self.cacheSize()), self.addressesArray(), parseInt(self.sets()))
+    cacheManagement.associativeCache(false)
+    self.iterateOnCache()
+    document.getElementById('menu').className = 'hide'
+    location.href = "#cacheGame"
+    document.getElementById('cacheGame').className = 'show'
+  }
 
+  self.cacheFullAssociative = function() {
+    self.contentCacheTable([])
+    cacheManagement = new CacheMemoryManagement(parseInt(self.cacheSize()), self.addressesArray(), self.cacheSize())
+    cacheManagement.associativeCache(true)
+    self.iterateOnCache()
+    document.getElementById('menu').className = 'hide'
+    location.href = "#cacheGame"
+    document.getElementById('cacheGame').className = 'show'
+  }
 
   self.stop = function() {
     realIndex = indexCache  === 0 ? self.cacheSize() - 1 : indexCache - 1
@@ -146,7 +233,7 @@ function CacheMemoryVM() {
         }
         if (self.currentAddress == cacheManagement.ramAddresses.length) {
           $.notify({
-            message: 'wait a second please... '
+            message: 'You Win!!'
           })
           clearInterval(self.thread)
           setTimeout(function() {
